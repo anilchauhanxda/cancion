35699ed01bcc3203d033179979152f4a68b11b74
Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>
2020-05-14 10:32:23 +0000

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -7,6 +7,7 @@ class Config(object):
  
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    TOKEN = os.environ.get("TOKEN",None)
     DEEZER_BOT = os.environ.get("DEEZER_BOT", "@DeezLoadBot")
     SONGS_CHANN = os.environ.get("SONGS_CHANN", "@songsxd")
     HU_STRING_SESSION = os.environ.get("HU_STRING_SESSION", None)
